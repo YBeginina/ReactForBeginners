595dc3edc7db493348a370d645fc8af490c34486
YBeginina <wraith16@yandex.ru>
2022-04-18 19:47:16 +0300

    add input & input button

--- my-app/src/ListOfWishes.tsx
+++ my-app/src/ListOfWishes.tsx
@@ -21,22 +21,34 @@ function ListOfWishes(props:ArrOfWishesPropsType){
     const filterWishHandler = (filter: FilterValuesType) => {
         return () => props.changeFilter(filter);
     }
-    const cssAllFilter = props.filter === 'All' ? `${cssL.activeFilter} ${cssL.filterButton}`: `${cssL.filterButton}`;
-    const cssRealisedFilter = props.filter === 'Realised' ? `${cssL.activeFilter} ${cssL.filterButton}`: `${cssL.filterButton}`;
-    const cssUnrealizedFilter = props.filter === 'Unrealized' ? `${cssL.activeFilter} ${cssL.filterButton}`: `${cssL.filterButton}`;
 
-    let wishesForRender: WishesType[];
-    switch (props.filter) {
-        case 'Realised':
-            wishesForRender = props.wishes.filter(w => w.isDone);
-            break;
-        case 'Unrealized':
-            wishesForRender = props.wishes.filter(w => !w.isDone);
-            break;
-        default:
-            wishesForRender = props.wishes;
-            break;
+    const cssAllFilter = props.filter === 'All' ?
+    `${cssL.filterButton} ${cssL.activeFilter}`:
+    `${cssL.filterButton}`;
+    const cssRealisedFilter = props.filter === 'Realised' ?
+    `${cssL.filterButton} ${cssL.activeFilter}`:
+    `${cssL.filterButton}`;
+    const cssUnrealizedFilter = props.filter === 'Unrealized' ?
+    `${cssL.filterButton} ${cssL.activeFilter}`:
+    `${cssL.filterButton}`;
+
+
+    const getWishesForRender = (wishes: WishesType[], filter: FilterValuesType) => {
+        let wishesForRender: WishesType[];
+        switch (props.filter) {
+            case 'Realised':
+                wishesForRender = props.wishes.filter(w => w.isDone);
+                break;
+            case 'Unrealized':
+                wishesForRender = props.wishes.filter(w => !w.isDone);
+                break;
+            default:
+                wishesForRender = props.wishes;
+                break;
+        }
+        return wishesForRender;
     }
+    const wishesForRender: WishesType[] = getWishesForRender(props.wishes, props.filter)
 
     const eachWish = wishesForRender.length ?
     wishesForRender.map(w => {
@@ -44,7 +56,9 @@ function ListOfWishes(props:ArrOfWishesPropsType){
         const removeWishHandler = () => props.removeWish(w.id);
 
         return (
-            <li className={cssL.wish} key={w.id}>
+            <li
+            className={cssL.wish}
+            key={w.id}>
                 <input type="checkbox" checked={w.isDone}/>
                 <span className={cssL.wishName}>{w.wishTitle}</span>
                 <button className={cssL.delButton} onClick={removeWishHandler}>X</button>
@@ -57,6 +71,11 @@ function ListOfWishes(props:ArrOfWishesPropsType){
       <div className={cssL.wishList}>
             <div className={cssL.title}>
                 {props.title}
+            </div>
+            <div className={cssL.inputLine}>
+                <input className={cssL.input}>
+                </input>
+                <button className={cssL.inputButton}>Add</button>
             </div>
             <div>
                 <ul className={cssL.list}>
